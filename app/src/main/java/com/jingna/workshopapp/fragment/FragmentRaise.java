e4ef3d09caea4a7c8deefba80ef314359ac627db
1455426414 <39983872+1455426414@users.noreply.github.com>
2019-06-18 15:27:07 +0800

    撒大大

--- app/src/main/java/com/jingna/workshopapp/fragment/FragmentRaise.java
+++ app/src/main/java/com/jingna/workshopapp/fragment/FragmentRaise.java
@@ -1,14 +1,34 @@
 package com.jingna.workshopapp.fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jingna.workshopapp.R;
+import com.jingna.workshopapp.adapter.GoodsDetailsViewpagerAdapter;
 import com.jingna.workshopapp.base.BaseFragment;
+import com.jingna.workshopapp.customview.ScaleTransitionPagerTitleView;
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,14 +36,68 @@ import butterknife.ButterKnife;
  */
 
 public class FragmentRaise extends BaseFragment {
-
+    private List<Fragment> fragmentList;
+    private ArrayList<String> mTitleDataList;
+    @BindView(R.id.magic_indicator)
+    MagicIndicator magicIndicator;
+    private FragmentManager mFragmentManager;
+    @BindView(R.id.vp)
+    ViewPager mViewPager;
+    private int index = 0;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_raise, null);
-
         ButterKnife.bind(this, view);
-
+        mFragmentManager = getActivity().getSupportFragmentManager();
+        initData();
         return view;
+    }
+    private void initData() {
+       fragmentList = new ArrayList<>();
+        fragmentList.add(new FragmentTuijian());
+        fragmentList.add(new FragmentTuijian());
+        fragmentList.add(new FragmentTuijian());
+        fragmentList.add(new FragmentTuijian());
+        GoodsDetailsViewpagerAdapter mViewPagerFragmentAdapter = new GoodsDetailsViewpagerAdapter(mFragmentManager, fragmentList);
+        mViewPager.setAdapter(mViewPagerFragmentAdapter);
+        mTitleDataList = new ArrayList<>();
+        mTitleDataList.add("平台推荐");
+        mTitleDataList.add("项目");
+        mTitleDataList.add("厂房");
+        mTitleDataList.add("车间");
+        CommonNavigator commonNavigator = new CommonNavigator(getContext());
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return mTitleDataList == null ? 0 : mTitleDataList.size();
+            }
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                SimplePagerTitleView simplePagerTitleView = new ScaleTransitionPagerTitleView(context);
+                simplePagerTitleView.setText(mTitleDataList.get(index));
+                //设置字体
+                simplePagerTitleView.setPadding(70,0,70,0);
+                simplePagerTitleView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15);
+                simplePagerTitleView.setNormalColor(Color.parseColor("#999999"));
+                simplePagerTitleView.setSelectedColor(Color.parseColor("#333333"));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                indicator.setColors(Color.parseColor("#000000"));
+                return indicator;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
+        mViewPager.setCurrentItem(index);
     }
 }
