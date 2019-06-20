ecbd42ac1e450344951747b0e0832032e0babb56
1455426414 <39983872+1455426414@users.noreply.github.com>
2019-06-20 14:36:52 +0800

    485915915941594159

--- app/src/main/java/com/jingna/workshopapp/adapter/FragmentCrowdTuijianAdapter.java
+++ app/src/main/java/com/jingna/workshopapp/adapter/FragmentCrowdTuijianAdapter.java
@@ -1,12 +1,14 @@
 package com.jingna.workshopapp.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jingna.workshopapp.R;
+import com.jingna.workshopapp.page.CrowdDetailsActivity;
 
 import java.util.List;
 
@@ -32,7 +34,14 @@ public class FragmentCrowdTuijianAdapter extends RecyclerView.Adapter<FragmentCr
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(context,CrowdDetailsActivity.class);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
