c05708f2028b1cfa155a34975c2f5ecf6ee24317
huweidong <466463054@qq.com>
2019-06-20 11:24:31 +0800

    65433465

--- app/src/main/java/com/jingna/workshopapp/fragment/FragmentIndex.java
+++ app/src/main/java/com/jingna/workshopapp/fragment/FragmentIndex.java
@@ -2,17 +2,22 @@ package com.jingna.workshopapp.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.jingna.workshopapp.R;
+import com.jingna.workshopapp.adapter.IndexAdapter;
 import com.jingna.workshopapp.base.BaseFragment;
+import com.jingna.workshopapp.util.StatusBarUtils;
 import com.jingna.workshopapp.util.ToastUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -21,11 +26,18 @@ import butterknife.ButterKnife;
 
 public class FragmentIndex extends BaseFragment {
 
+    @BindView(R.id.rv)
+    RecyclerView recyclerView;
+
+    private IndexAdapter adapter;
+    private List<String> mList;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_index, null);
 
+        StatusBarUtils.setStatusBarTransparent(getActivity());
         ButterKnife.bind(this, view);
         initData();
 
@@ -34,13 +46,29 @@ public class FragmentIndex extends BaseFragment {
 
     private void initData() {
 
-        List<String> list = new ArrayList<>();
-        list.add("0");
-        list.add("1");
-        list.add("2");
-        if(list.contains("2")){
-            ToastUtil.showShort(getContext(), "有");
-        }
+        mList = new ArrayList<>();
+        mList.add("");
+        mList.add("");
+        mList.add("");
+        mList.add("");
+        adapter = new IndexAdapter(mList);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext()){
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
 
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if(!hidden){
+            StatusBarUtils.setStatusBarTransparent(getActivity());
+        }
     }
 }
