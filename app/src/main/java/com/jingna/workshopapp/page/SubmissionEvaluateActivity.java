93b945859c536363a151be91b459c93d9cb4cc51
1455426414 <39983872+1455426414@users.noreply.github.com>
2019-08-08 17:38:36 +0800

    4854854545

---
+++ app/src/main/java/com/jingna/workshopapp/page/SubmissionEvaluateActivity.java
@@ -0,0 +1,15 @@
+package com.jingna.workshopapp.page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jingna.workshopapp.R;
+
+public class SubmissionEvaluateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submission_evaluate);
+    }
+}
