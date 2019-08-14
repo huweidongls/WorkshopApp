8a93d5a120140700797ffbb39c59c7f576fe77c6
1455426414 <39983872+1455426414@users.noreply.github.com>
2019-08-14 17:35:30 +0800

    1121212112

--- app/src/main/java/com/jingna/workshopapp/page/CrowdDetailsSupportActivity.java
+++ app/src/main/java/com/jingna/workshopapp/page/CrowdDetailsSupportActivity.java
@@ -15,6 +15,8 @@ import com.jingna.workshopapp.bean.AddressBean;
 import com.jingna.workshopapp.net.NetUrl;
 import com.jingna.workshopapp.util.StatusBarUtils;
 
+import java.util.Map;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -33,9 +35,13 @@ public class CrowdDetailsSupportActivity extends BaseActivity {
     TextView tvPhonenum;
     @BindView(R.id.tv_address)
     TextView tvAddress;
+    @BindView(R.id.tv_invoice)
+    TextView tv_invoice;
     private int pay=1;
     private int paycheck=0;
     private int addressid=0;
+    private int invoiceId=0;
+    private Map<String, String> map;//发票map
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +49,7 @@ public class CrowdDetailsSupportActivity extends BaseActivity {
         StatusBarUtils.setStatusBar(CrowdDetailsSupportActivity.this, getResources().getColor(R.color.statusbar_color));
         ButterKnife.bind(CrowdDetailsSupportActivity.this);
     }
-    @OnClick({R.id.rl_back, R.id.iv_wx,R.id.iv_zfb,R.id.iv_check,R.id.rl_address})
+    @OnClick({R.id.rl_back, R.id.iv_wx,R.id.iv_zfb,R.id.iv_check,R.id.rl_address,R.id.ll_invoice})
     public void onClick(View view){
         Intent intent = new Intent();
         switch (view.getId()){
@@ -53,12 +59,12 @@ public class CrowdDetailsSupportActivity extends BaseActivity {
             case R.id.iv_zfb:
                 pay=2;
                 Glide.with(context).load(R.mipmap.duihao).into(iv_pay_zfb);
-                Glide.with(context).load("#ffabd9ff").into(iv_pay_wx);
+                Glide.with(context).load(R.drawable.img_radios).into(iv_pay_wx);
                 break;
             case R.id.iv_wx:
                 pay=1;
                 Glide.with(context).load(R.mipmap.duihao).into(iv_pay_wx);
-                Glide.with(context).load("#ffabd9ff").into(iv_pay_zfb);
+                Glide.with(context).load(R.drawable.img_radios).into(iv_pay_zfb);
                 break;
             case R.id.iv_check:
                 if (paycheck==0){
@@ -74,6 +80,11 @@ public class CrowdDetailsSupportActivity extends BaseActivity {
                 intent.putExtra("type", "order");
                 startActivityForResult(intent, 1001);
                 break;
+            case R.id.ll_invoice:
+                intent.setClass(context, InvoiceActivity.class);
+                intent.putExtra("price", 0.00);
+                startActivityForResult(intent, 100);
+                break;
         }
     }
     @Override
@@ -85,6 +96,10 @@ public class CrowdDetailsSupportActivity extends BaseActivity {
             tvName.setText(bean.getConsignee());
             tvPhonenum.setText(bean.getConsigneeTel());
             tvAddress.setText(bean.getLocation()+bean.getAdress());
+        }else if(resultCode == 101&&data != null){
+            invoiceId = 1;
+            tv_invoice.setText("开发票");
+            map = (Map<String, String>) data.getSerializableExtra("map");
         }
     }
 }
