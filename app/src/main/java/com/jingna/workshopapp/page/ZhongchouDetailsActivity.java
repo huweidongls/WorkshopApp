305d7d15b24bf172b21ceb1faea59aa911149c0e
huweidong <466463054@qq.com>
2019-08-16 17:30:07 +0800

    456456

--- app/src/main/java/com/jingna/workshopapp/page/ZhongchouDetailsActivity.java
+++ app/src/main/java/com/jingna/workshopapp/page/ZhongchouDetailsActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -14,6 +16,7 @@ import android.webkit.WebViewClient;
 import com.jingna.workshopapp.R;
 import com.jingna.workshopapp.base.BaseActivity;
 import com.jingna.workshopapp.util.StatusBarUtils;
+import com.jingna.workshopapp.util.ToastUtil;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,11 +29,14 @@ public class ZhongchouDetailsActivity extends BaseActivity {
     @BindView(R.id.webview)
     WebView webview;
 
+    private String id = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zhongchou_details);
 
+        id = getIntent().getStringExtra("id");
         StatusBarUtils.setStatusBar(ZhongchouDetailsActivity.this, getResources().getColor(R.color.white_ffffff));
         ButterKnife.bind(ZhongchouDetailsActivity.this);
         initWebView();
@@ -63,15 +69,39 @@ public class ZhongchouDetailsActivity extends BaseActivity {
             }
         });
 
-        webview.loadUrl("http://192.168.2.121/detail.html");
+        webview.addJavascriptInterface(new JsInterface(), "android");
+
+        webview.loadUrl("http://192.168.2.121/detail.html?id="+id);
 
     }
 
+    public class JsInterface {
+        //JS中调用Android中的方法 和返回值处理的一种方法
+
+        /****
+         * Html中的点击事件 onclick
+         * <input type="button" value="结算" onclick="showToast('12')">
+         *
+         * @param
+         */
+        @JavascriptInterface
+        public void toDetail(String targetid) {
+            ToastUtil.showShort(context, targetid);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @OnClick({R.id.rl_back})
     public void onClick(View view){
         switch (view.getId()){
             case R.id.rl_back:
                 finish();
+//                webview.evaluateJavascript("javascript:getDetailId(123)", new ValueCallback<String>() {
+//                    @Override
+//                    public void onReceiveValue(String value) {
+//                        //此处为 js 返回的结果
+//                    }
+//                });
                 break;
         }
     }
