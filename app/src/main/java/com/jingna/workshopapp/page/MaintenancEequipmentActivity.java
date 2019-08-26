304351c7c2a3d707cd764cf48a88f6180937c791
1455426414 <39983872+1455426414@users.noreply.github.com>
2019-08-26 10:12:09 +0800

    85588585

---
+++ app/src/main/java/com/jingna/workshopapp/page/MaintenancEequipmentActivity.java
@@ -0,0 +1,15 @@
+package com.jingna.workshopapp.page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jingna.workshopapp.R;
+
+public class MaintenancEequipmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintenanc_eequipment);
+    }
+}
